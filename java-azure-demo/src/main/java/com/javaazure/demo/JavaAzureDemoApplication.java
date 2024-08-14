bf505e8cbaf812a38e9fe8d37d39102ac33b04ac
gmukh1503 <gourab.mukherjee3@gmail.com>
2024-08-14 08:31:14 +0530

    First Commit

---
+++ java-azure-demo/src/main/java/com/javaazure/demo/JavaAzureDemoApplication.java
@@ -0,0 +1,20 @@
+package com.javaazure.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JavaAzureDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAzureDemoApplication.class, args);
+	}
+
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Your application is deployed and running in Azure AppService.";
+	}
+}
---
+++ java-azure-demo/src/test/java/com/javaazure/demo/JavaAzureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaazure.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAzureDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
